07a81aacd9d3966c41b0b1daf116f8b91bc0f9dc
Danilo N. B. <danilobenatti@gmail.com>
2023-09-02 17:48:48 -0300

    Initial code, first steps.

---
+++ src/beginners_level/Exercise001.java
@@ -0,0 +1,10 @@
+package beginners_level;
+
+public class Exercise001 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My first code in java.");
+	}
+	
+}
